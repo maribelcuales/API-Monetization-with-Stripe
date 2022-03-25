96542d032f193c5fcba1893d30080cc14168d12e
maribelcuales <maribelcuales@gmail.com>
2022-03-25 22:05:34 +0800

    Built a basic API endpoint.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-import 'dotenv/config';
-const express = require('express');
-const app = express(); 
+import "dotenv/config";
+const express = require("express");
+const app = express();
 
-const stripe = require('stripe')(process.env.SK_TEST-KEY);
+const stripe = require("stripe")(process.env.SK_TEST - KEY);
 
+// Make a call to the API
+app.get('/api', async (req, res) => {
+  const { apiKey } = req.query;
+
+  if (!apiKey) {
+    res.sendStatus(400); // bad request
+  }
+
+  // TODO validate apiKey
+  res.send({ data: "🔥🔥🔥🔥🔥🔥🔥🔥" });
+});
