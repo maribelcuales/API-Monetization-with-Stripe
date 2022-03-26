95b9cb4def465e3c921954d6ce8b9c4dd8da1ca5
maribelcuales <maribelcuales@gmail.com>
2022-03-26 21:08:39 +0800

    Create mock database in memory. For modelling later in preferred database.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import "dotenv/config";
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const stripe = require("stripe")(process.env.SK_TEST - KEY);
+const stripe = require("stripe")(process.env.SK_TEST_KEY);
 
 // Make a call to the API
-app.get('/api', async (req, res) => {
+app.get("/api", async (req, res) => {
   const { apiKey } = req.query;
 
   if (!apiKey) {
@@ -15,3 +15,19 @@ app.get('/api', async (req, res) => {
   // TODO validate apiKey
   res.send({ data: "🔥🔥🔥🔥🔥🔥🔥🔥" });
 });
+
+// Reverse mapping of stripe to API key
+// To model in preferred database
+const customers = {
+  // stripeCustomerId: data
+  stripeCustomerId: {
+    apiKey: "123xyz",
+    active: false,
+    itemId: "stripeSubscriptionItemId",
+  },
+};
+
+const apiKeys = {
+  // apiKey: customerData
+  "123xyz": "stripeCustomerId",
+};
